edb8447b3dd29e8a566385d8d697fdf870759cd3
kkrann1222 <karansinghhhks66@gmail.com>
2025-05-16 21:03:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+import express from "express"
+import mongoose  from "mongoose"
+import dotenv, { config } from "dotenv"
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 7000; 
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL).then(()=>{
+    console.log("Databse is connnected successfully");
+    app.listen(PORT,() =>{
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((error) => console.log(error));
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+});
+
+const UserModel = mongoose.model("users",userSchema)
+
+app.get("/getUsers", async(req,res) => {
+    const userData = await UserModel.find();
+    res.json(userData);
+})
